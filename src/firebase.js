4ef191d6b484f1c6e523978552d479e9778da7d3
Abdullah Khan Kakar <2022bbit71@student.uet.edu.pk>
2025-10-25 14:16:47 +0500

    initialize Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANqm01cwwkYWY0DNuY5aRR-uH-dLgrASk",
+  authDomain: "netflix-clone-c7eab.firebaseapp.com",
+  projectId: "netflix-clone-c7eab",
+  storageBucket: "netflix-clone-c7eab.firebasestorage.app",
+  messagingSenderId: "6532787929",
+  appId: "1:6532787929:web:00a3653f26db01ea5098c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
